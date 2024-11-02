e3546b59bc0d68d1d3add144bbb1a04783943ead
Tung Pham <tung.42@gmail.com>
2024-11-03 06:56:49 +0700

    page

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 import CourseCard from "./CourseCard";
 import FilterBar from "./FilterBar";
 
@@ -9,8 +11,6 @@ const TOKEN = "eTg2dlAvSmJmREVHZktLWjI0enRuUT09";
 const DEFAULT_CATEGORY_ID = 1;
 
 const CourseList = () => {
-  // eslint-disable-next-line
-  const [courses, setCourses] = useState([]);
   const [filteredCourses, setFilteredCourses] = useState([]);
   const [error, setError] = useState(null);
   const [pagination, setPagination] = useState({
@@ -19,102 +19,157 @@ const CourseList = () => {
   });
   const [totalPages, setTotalPages] = useState(10);
 
-  const fetchCourses = useCallback(async (categoryId, pageNumber = 1) => {
+  const fetchCourses = useCallback(async () => {
     try {
-      const response = await axios.get("https://unica.vn/api/coursecategory", {
+      const { data } = await axios.get("https://unica.vn/api/coursecategory", {
         params: {
-          category_id: categoryId,
+          category_id: pagination.categoryId,
           aff_id: AFFILIATE_ID,
           token: TOKEN,
-          page: pageNumber,
+          page: pagination.page,
         },
       });
-      const coursesData = response.data?.data?.course || [];
-      setCourses(coursesData);
-      setFilteredCourses(coursesData);
+      setFilteredCourses(data?.data?.course || []);
     } catch {
       setError("Error fetching courses. Please try again later.");
     }
-  }, []);
+  }, [pagination]);
 
-  const fetchTotalPages = useCallback(async (categoryId) => {
+  const fetchTotalPages = useCallback(async () => {
     let currentPage = 1;
     let lastPage = 1;
-    let isSuccess = true;
 
     try {
-      while (isSuccess) {
-        const response = await axios.get(
+      while (true) {
+        const { data } = await axios.get(
           "https://unica.vn/api/coursecategory",
           {
             params: {
-              category_id: categoryId,
+              category_id: pagination.categoryId,
               aff_id: AFFILIATE_ID,
               token: TOKEN,
               page: currentPage,
             },
           }
         );
-        isSuccess = response.data?.success === 1;
 
-        if (isSuccess) {
-          lastPage = currentPage;
-          currentPage++;
-        }
+        if (data?.success !== 1) break;
+        lastPage = currentPage;
+        currentPage++;
       }
       setTotalPages(lastPage);
     } catch {
       setError("Error fetching total pages. Please try again later.");
     }
-  }, []);
+  }, [pagination.categoryId]);
 
   useEffect(() => {
-    fetchTotalPages(pagination.categoryId);
-    fetchCourses(pagination.categoryId, pagination.page);
-  }, [fetchCourses, fetchTotalPages, pagination]);
+    fetchTotalPages();
+    fetchCourses();
+  }, [fetchCourses, fetchTotalPages]);
 
   const handleFilterChange = ({ categoryId }) => {
-    setPagination({ page: 1, categoryId }); // Reset to first page and update categoryId
-    fetchTotalPages(categoryId);
-    fetchCourses(categoryId, 1);
+    setPagination({ page: 1, categoryId });
   };
 
-  const nextPage = () => {
-    if (pagination.page < totalPages) {
-      setPagination((prev) => ({ ...prev, page: prev.page + 1 }));
-    }
-  };
-
-  const prevPage = () => {
-    if (pagination.page > 1) {
-      setPagination((prev) => ({ ...prev, page: prev.page - 1 }));
-    }
+  const handlePageChange = (pageNumber) => {
+    setPagination((prev) => ({ ...prev, page: pageNumber }));
   };
 
   if (error) return <p>{error}</p>;
 
-  return (
-    <Container className="my-4">
-      <FilterBar onFilterChange={handleFilterChange} />
+  const PaginationControls = () => {
+    const pageButtons = [];
+
+    // Always show the first page button
+    pageButtons.push(
+      <Button
+        key={1}
+        onClick={() => handlePageChange(1)}
+        variant={pagination.page === 1 ? "primary" : "outline-primary"}
+        className="mx-1"
+      >
+        1
+      </Button>
+    );
+
+    // Show ellipsis if the current page is beyond the 3rd page
+    if (pagination.page > 3) {
+      pageButtons.push(
+        <FontAwesomeIcon
+          icon={faEllipsisH}
+          key="start-ellipsis"
+          className="mx-2"
+        />
+      );
+    }
+
+    // Display the current page, the one before, and the one after
+    for (
+      let i = Math.max(2, pagination.page - 1);
+      i <= Math.min(totalPages - 1, pagination.page + 1);
+      i++
+    ) {
+      pageButtons.push(
+        <Button
+          key={i}
+          onClick={() => handlePageChange(i)}
+          variant={pagination.page === i ? "primary" : "outline-primary"}
+          className="mx-1"
+        >
+          {i}
+        </Button>
+      );
+    }
+
+    // Show ellipsis if there are pages remaining before the last page
+    if (pagination.page < totalPages - 2) {
+      pageButtons.push(
+        <FontAwesomeIcon
+          icon={faEllipsisH}
+          key="end-ellipsis"
+          className="mx-2"
+        />
+      );
+    }
+
+    // Always show the last page button
+    pageButtons.push(
+      <Button
+        key={totalPages}
+        onClick={() => handlePageChange(totalPages)}
+        variant={pagination.page === totalPages ? "primary" : "outline-primary"}
+        className="mx-1"
+      >
+        {totalPages}
+      </Button>
+    );
+
+    return (
       <div className="d-flex justify-content-center my-4 align-items-center">
         <Button
-          onClick={prevPage}
+          onClick={() => handlePageChange(pagination.page - 1)}
           disabled={pagination.page === 1}
           className="me-2"
         >
           Trước
         </Button>
-        <span>
-          Trang {pagination.page} trong {totalPages}
-        </span>
+        <div className="d-flex align-items-center">{pageButtons}</div>
         <Button
-          onClick={nextPage}
+          onClick={() => handlePageChange(pagination.page + 1)}
           disabled={pagination.page === totalPages}
           className="ms-2"
         >
           Sau
         </Button>
       </div>
+    );
+  };
+
+  return (
+    <Container className="my-4">
+      <FilterBar onFilterChange={handleFilterChange} />
+      <PaginationControls />
       <Row>
         {filteredCourses.map((course) => (
           <Col key={course.id} md={6} className="mb-4">
@@ -122,25 +177,7 @@ const CourseList = () => {
           </Col>
         ))}
       </Row>
-      <div className="d-flex justify-content-center my-4 align-items-center">
-        <Button
-          onClick={prevPage}
-          disabled={pagination.page === 1}
-          className="me-2"
-        >
-          Trước
-        </Button>
-        <span>
-          Trang {pagination.page} trong {totalPages}
-        </span>
-        <Button
-          onClick={nextPage}
-          disabled={pagination.page === totalPages}
-          className="ms-2"
-        >
-          Sau
-        </Button>
-      </div>
+      <PaginationControls />
     </Container>
   );
 };
