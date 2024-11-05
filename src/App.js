ed79c955946d97c0a993d79e190e71f422bb270d
Tung Pham <tung.42@gmail.com>
2024-11-05 15:41:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 import CourseList from "./components/CourseList";
 
 const App = () => {
   return (
     <div className="text-center my-4">
       <h1>
-        <FontAwesomeIcon icon={faBook} className="me-2" /> Khóa học Online
+        <FontAwesomeIcon icon={faGraduationCap} className="me-2" /> Khóa học
+        Online
       </h1>
       <CourseList />
     </div>
